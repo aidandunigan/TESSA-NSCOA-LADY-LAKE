77bfa3bdebfa0435992dbeb0cd6ef0b7295cacb2
aidandunigan <aidanduniga1@gmail.com>
2025-05-22 13:55:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-<!-- Zapier Chatbot Embed -->
-<script async type="module" src="https://interfaces.zapier.com/assets/web-components/zapier-interfaces/zapier-interfaces.esm.js"></script>
-<zapier-interfaces-chatbot-embed
-  is-popup="false"
-  chatbot-id="cmazkbg330044l78v1wwaxfl9"
-  height="600px"
-  width="400px">
-</zapier-interfaces-chatbot-embed>
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const { Configuration, OpenAIApi } = require('openai');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+app.post('/api/chat', async (req, res) => {
+  try {
+    const messages = req.body.messages;
+    const response = await openai.createChatCompletion({
+      model: "gpt-4",
+      messages: messages,
+    });
+    res.json({ reply: response.data.choices[0].message.content });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(3001, () => {
+  console.log('Server running on http://localhost:3001');
+});
 
